50c6ea33773b6f08f57f07754ead1b7d30abcdd5
Sean Radke <sradke2035@gmail.com>
2023-02-07 13:47:08 -0600

    UAV-LSM6DSO32 WORKING CODE

--- UAV/src/main.cpp
+++ UAV/src/main.cpp
@@ -1,42 +1,183 @@
-#include <Arduino.h>
-#include <SPI.h>
-#include "Adafruit_BMP3XX.h"
-#define BMP_CS 6
-#define SEALEVELPRESSURE_HPA (1013.25)
-Adafruit_BMP3XX bmp; //bmp390
+#include <arduino.h>
+#include <Adafruit_LSM6DSO32.h>
 
-void setup() 
-{
-  Serial.begin(9600);
-  if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
-    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
-    while (1);
+#define LSM_CS 9  // For SPI mode, we need a CS pin
+
+Adafruit_LSM6DSO32 dso32;
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+     if (!dso32.begin_SPI(LSM_CS)) {
+     Serial.println("Failed to find LSM6DSO32 chip");
+    while (1) {
+      delay(10);
+    }
   }
-  // Set up oversampling and filter initialization
-  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
-  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
-  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+
+  dso32.setAccelRange(LSM6DSO32_ACCEL_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (dso32.getAccelRange()) {
+  case LSM6DSO32_ACCEL_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case LSM6DSO32_ACCEL_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case LSM6DSO32_ACCEL_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  case LSM6DSO32_ACCEL_RANGE_32_G:
+    Serial.println("+-32G");
+    break;
+  }
+
+  // dso32.setGyroRange(LSM6DS_GYRO_RANGE_250_DPS );
+  Serial.print("Gyro range set to: ");
+  switch (dso32.getGyroRange()) {
+  case LSM6DS_GYRO_RANGE_125_DPS:
+    Serial.println("125 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_250_DPS:
+    Serial.println("250 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_500_DPS:
+    Serial.println("500 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_1000_DPS:
+    Serial.println("1000 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_2000_DPS:
+    Serial.println("2000 degrees/s");
+    break;
+  case ISM330DHCX_GYRO_RANGE_4000_DPS:
+    break; // unsupported range for the DSO32
+  }
+
+  // dso32.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
+  Serial.print("Accelerometer data rate set to: ");
+  switch (dso32.getAccelDataRate()) {
+  case LSM6DS_RATE_SHUTDOWN:
+    Serial.println("0 Hz");
+    break;
+  case LSM6DS_RATE_12_5_HZ:
+    Serial.println("12.5 Hz");
+    break;
+  case LSM6DS_RATE_26_HZ:
+    Serial.println("26 Hz");
+    break;
+  case LSM6DS_RATE_52_HZ:
+    Serial.println("52 Hz");
+    break;
+  case LSM6DS_RATE_104_HZ:
+    Serial.println("104 Hz");
+    break;
+  case LSM6DS_RATE_208_HZ:
+    Serial.println("208 Hz");
+    break;
+  case LSM6DS_RATE_416_HZ:
+    Serial.println("416 Hz");
+    break;
+  case LSM6DS_RATE_833_HZ:
+    Serial.println("833 Hz");
+    break;
+  case LSM6DS_RATE_1_66K_HZ:
+    Serial.println("1.66 KHz");
+    break;
+  case LSM6DS_RATE_3_33K_HZ:
+    Serial.println("3.33 KHz");
+    break;
+  case LSM6DS_RATE_6_66K_HZ:
+    Serial.println("6.66 KHz");
+    break;
+  }
+
+  // dso32.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
+  Serial.print("Gyro data rate set to: ");
+  switch (dso32.getGyroDataRate()) {
+  case LSM6DS_RATE_SHUTDOWN:
+    Serial.println("0 Hz");
+    break;
+  case LSM6DS_RATE_12_5_HZ:
+    Serial.println("12.5 Hz");
+    break;
+  case LSM6DS_RATE_26_HZ:
+    Serial.println("26 Hz");
+    break;
+  case LSM6DS_RATE_52_HZ:
+    Serial.println("52 Hz");
+    break;
+  case LSM6DS_RATE_104_HZ:
+    Serial.println("104 Hz");
+    break;
+  case LSM6DS_RATE_208_HZ:
+    Serial.println("208 Hz");
+    break;
+  case LSM6DS_RATE_416_HZ:
+    Serial.println("416 Hz");
+    break;
+  case LSM6DS_RATE_833_HZ:
+    Serial.println("833 Hz");
+    break;
+  case LSM6DS_RATE_1_66K_HZ:
+    Serial.println("1.66 KHz");
+    break;
+  case LSM6DS_RATE_3_33K_HZ:
+    Serial.println("3.33 KHz");
+    break;
+  case LSM6DS_RATE_6_66K_HZ:
+    Serial.println("6.66 KHz");
+    break;
+  }
 }
 
-void loop() 
-{
-   if (! bmp.performReading()) {
-    Serial.println("Failed to perform reading :(");
-    return;
-  }
-bmp.performReading();
-    Serial.println();
-    Serial.print("Temp: ");
-    Serial.print((bmp.temperature * 1.8) + 32);
-    Serial.println(" F");
-    Serial.println();
-    Serial.print("Pres.: ");
-    Serial.print(bmp.pressure / 100.0);
-    Serial.println(" hPa");
-    Serial.println();
-    Serial.print("Alt: ");
-    Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA) * 3.28084);
-    Serial.println(" ft");
-    delay(1000); // not necessary - just to slow down readings
+void loop() {
+
+  //  /* Get a new normalized sensor event */
+  sensors_event_t accel;
+  sensors_event_t gyro;
+  sensors_event_t temp;
+  dso32.getEvent(&accel, &gyro, &temp);
+
+  Serial.print("\t\tTemperature ");
+  Serial.print(temp.temperature);
+  Serial.println(" deg C");
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tAccel X: ");
+  Serial.print(accel.acceleration.x);
+  Serial.print(" \tY: ");
+  Serial.print(accel.acceleration.y);
+  Serial.print(" \tZ: ");
+  Serial.print(accel.acceleration.z);
+  Serial.println(" m/s^2 ");
+
+  /* Display the results (rotation is measured in rad/s) */
+  Serial.print("\t\tGyro X: ");
+  Serial.print(gyro.gyro.x);
+  Serial.print(" \tY: ");
+  Serial.print(gyro.gyro.y);
+  Serial.print(" \tZ: ");
+  Serial.print(gyro.gyro.z);
+  Serial.println(" radians/s ");
+  Serial.println();
+
+  delay(100);
+
+  //  // serial plotter friendly format
+
+  //  Serial.print(temp.temperature);
+  //  Serial.print(",");
+
+  //  Serial.print(accel.acceleration.x);
+  //  Serial.print(","); Serial.print(accel.acceleration.y);
+  //  Serial.print(","); Serial.print(accel.acceleration.z);
+  //  Serial.print(",");
+
+  // Serial.print(gyro.gyro.x);
+  // Serial.print(","); Serial.print(gyro.gyro.y);
+  // Serial.print(","); Serial.print(gyro.gyro.z);
+  // Serial.println();
+  //  delayMicroseconds(10000);
 }
