9a5f374a5c4d4df2976e868614d5271ebecafe0a
Sean Radke <sradke2035@gmail.com>
2023-02-07 13:27:27 -0600

    BMP390 System working - this is the necessary code

--- UAV/src/main.cpp
+++ UAV/src/main.cpp
@@ -1,52 +1,42 @@
 #include <Arduino.h>
-#include <Wire.h>
 #include <SPI.h>
-#include <servo.h>
-
-//#include <I2C_Anything.h>
-//#include <Adafruit_Sensor.h>
-//#include <Gyro>
-//#include <radio>
-//#include <gps>
-//#include "Adafruit_BMP3XX.h"
-
-  #define SEALEVELPRESSURE_HPA (1013.25)
-// Adafruit_BMP3XX bmp; //bmp390
-
-int output = 1024;
-
+#include "Adafruit_BMP3XX.h"
+#define BMP_CS 6
+#define SEALEVELPRESSURE_HPA (1013.25)
+Adafruit_BMP3XX bmp; //bmp390
 
 void setup() 
 {
-
-  Serial.begin(57600);
-  Serial.println("Initialized");
-  Wire.begin();
-  /*  
-  if(!bmp.begin_I2C()) //Int BMP390 - hardware I2C mode, can pass in address & alt Wire
-  { 
-    Serial.println("Could not find BMP390 - Error");
-    while (1); 
+  Serial.begin(9600);
+  if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
+    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+    while (1);
   }
-  */
-  analogWriteFrequency(2, 250);
-  analogWriteResolution(12);
-  delay(1250); 
-  analogWrite(2, 1024);
-  delay(2000);
-  analogWrite(2, 2047);
-  delay(2000);
-  analogWrite(2, 1024);
-  delay(3000);
+  // Set up oversampling and filter initialization
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 }
 
 void loop() 
 {
- //analogwrite(2,2047);
- 
-  if (output < 2047)
-  {
-    output += 1;
+   if (! bmp.performReading()) {
+    Serial.println("Failed to perform reading :(");
+    return;
   }
-  analogWrite(2, output);
+bmp.performReading();
+    Serial.println();
+    Serial.print("Temp: ");
+    Serial.print((bmp.temperature * 1.8) + 32);
+    Serial.println(" F");
+    Serial.println();
+    Serial.print("Pres.: ");
+    Serial.print(bmp.pressure / 100.0);
+    Serial.println(" hPa");
+    Serial.println();
+    Serial.print("Alt: ");
+    Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA) * 3.28084);
+    Serial.println(" ft");
+    delay(1000); // not necessary - just to slow down readings
 }
