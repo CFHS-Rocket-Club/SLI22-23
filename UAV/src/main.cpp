8026ef3fd66101413a745bcd15166128e3c23aff
Ethan Strohm <ethan.g.s@outlook.com>
2023-02-07 13:44:29 -0600

    drone motor output and arming

--- UAV/src/main.cpp
+++ UAV/src/main.cpp
@@ -1,52 +1,68 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
-#include <servo.h>
+#include <Servo.h>
 
-//#include <I2C_Anything.h>
-//#include <Adafruit_Sensor.h>
-//#include <Gyro>
-//#include <radio>
-//#include <gps>
-//#include "Adafruit_BMP3XX.h"
+// #include <I2C_Anything.h>
+// #include <Adafruit_Sensor.h>
+// #include <Gyro>
+// #include <radio>
+// #include <gps>
+// #include "Adafruit_BMP3XX.h"
 
-  #define SEALEVELPRESSURE_HPA (1013.25)
+#define SEALEVELPRESSURE_HPA (1013.25)
 // Adafruit_BMP3XX bmp; //bmp390
 
-int output = 1024;
+int output = 1000;
 
+Servo esc;
 
-void setup() 
+enum MotorMode
 {
+    ArmRampUp,
+    ArmRampDown,
+    Pause,
+    Operate,
+};
 
-  Serial.begin(57600);
-  Serial.println("Initialized");
-  Wire.begin();
-  /*  
-  if(!bmp.begin_I2C()) //Int BMP390 - hardware I2C mode, can pass in address & alt Wire
-  { 
-    Serial.println("Could not find BMP390 - Error");
-    while (1); 
-  }
-  */
-  analogWriteFrequency(2, 250);
-  analogWriteResolution(12);
-  delay(1250); 
-  analogWrite(2, 1024);
-  delay(2000);
-  analogWrite(2, 2047);
-  delay(2000);
-  analogWrite(2, 1024);
-  delay(3000);
+MotorMode motorMode = ArmRampUp;
+
+void setup()
+{
+    Serial.begin(57600);
+    Serial.println("Initialized");
+    Wire.begin();
+    /*
+    if(!bmp.begin_I2C()) //Int BMP390 - hardware I2C mode, can pass in address & alt Wire
+    {
+      Serial.println("Could not find BMP390 - Error");
+      while (1);
+    }
+    */
+    esc.attach(2);
+    delay(500);
+    Serial.println("Delay Complete");
 }
 
-void loop() 
+void loop()
 {
- //analogwrite(2,2047);
- 
-  if (output < 2047)
-  {
-    output += 1;
-  }
-  analogWrite(2, output);
+    // analogwrite(2,2047);
+
+    if (motorMode == ArmRampUp)
+    {
+        if (output < 1500)
+        {
+            output += 1;
+        }
+        else
+        {
+            output = 1490;
+            motorMode = Operate;
+            Serial.println("Ramp Up Done");
+        }
+    }
+
+    esc.writeMicroseconds(output);
+
+    delay(10);
 }
