cbfbfabb56a5e329bd1633a834814e556dce4eed
Sean Radke <sradke2035@gmail.com>
2023-04-14 12:15:57 -0500

    Deployment code is almost complete

--- Deployment/src/main.cpp
+++ Deployment/src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <Adafruit_BMP3XX.h>
 #include <AccelStepper.h>
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
@@ -9,28 +8,20 @@
 #include <RHReliableDatagram.h>
 #include <Servo.h>
 
-TinyGPSPlus gps;                         // GPS Driver
-RH_RF95 rf95(3, 2);                      // Singleton instance of the radio driver
-Adafruit_BMP3XX bmp = Adafruit_BMP3XX(); // Initializes the bmp3xx driver
-#define BMP_CS 4                         // Chip Select Pin
-#define SEALEVELPRESSURE_HPA (1013.25)   // Sea Level Pressure
-
-// Stepper Motors
-AccelStepper motor1(AccelStepper::DRIVER, 8, 9); //8 = pinStep and 9 = pinDirection
-AccelStepper motor2(AccelStepper::DRIVER, 14, 15);
-
-Servo servo1; // create servo object #1 to control servo 1
-Servo servo2; // create servo object #2 to control servo 2
-
-float currentmillis = 0; // Variable used to run different functions when a command is given
-bool separate = false;   // Variable used to run different functions when a command is given
+TinyGPSPlus gps;                                   // GPS Driver
+RH_RF95 rf95(3, 2);                                // Singleton instance of the radio driver
+AccelStepper motor1(AccelStepper::DRIVER, 8, 9);   // Stepper Motors: 8 = pinStep and 9 = pinDirection
+AccelStepper motor2(AccelStepper::DRIVER, 14, 15); // Stepper Motors: 14 = pinStep and 15 = pinDirection
+Servo servo1;                                      // create servo object #1 to control servo 1
+Servo servo2;                                      // create servo object #2 to control servo 2
+bool separate = false;                             // Variable used to run different functions when a command is given
 
 void sendposition()
 {
     Serial.println("Postrue");
     uint8_t data[24];
 
-    String lat = String("hlat") + String(gps.location.lat(), 8);
+    String lat = String("Rlat ") + String(gps.location.lat(), 8);
 
     for (int i = 0; i < lat.length(); i++)
     {
@@ -40,7 +31,7 @@ void sendposition()
 
     delay(5);
 
-    String lng = String("hlng") + String(gps.location.lng(), 8);
+    String lng = String("Rlng ") + String(gps.location.lng(), 8);
     for (int i = 0; i < lng.length(); i++)
     {
         data[i] = lng.charAt(i);
@@ -48,58 +39,33 @@ void sendposition()
     rf95.send(data, lng.length());
 
     delay(5);
-
-    String alt = String("halt") + String(bmp.readAltitude(SEALEVELPRESSURE_HPA) * 3.28084, 8);
-    for (int i = 0; i < lng.length(); i++)
-    {
-        data[i] = alt.charAt(i);
-    }
-    rf95.send(data, alt.length());
-
-    delay(5);
 }
 
 void setup()
 {
-    // Serial
-    Serial.begin(9600);
-    Serial1.begin(9600); // GPS
+    Serial.begin(9600);  // Serial Monitor
+    Serial1.begin(9600); // GPS hardware serial
 
-    // Radio
-    if (!rf95.init())
-        Serial.println("Radio init failed"); // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on...The default transmitter power is 13dBm, using PA_BOOST.
-    // rf95.setModemConfig(RH_RF95::Bw500Cr45Sf128); // You can change the modulation parameters with eg
-    // rf95.setTxPower(20, false); //The default transmitter power is 13dBm, using PA_BOOST. - If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then you can set transmitter powers from 2 to 20 dBm:
-
-    // BMP 3XX
-    if (!bmp.begin_SPI(BMP_CS)) // hardware SPI mode
+    if (!rf95.init()) // Radio Initialization
     {
-        Serial.println("Could not find a valid BMP3 sensor, check wiring!");
-    }
-    bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X); // Set up oversampling and filter initialization
-    bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-    bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
-    bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+        Serial.println("Radio init failed");
+    }                           // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on...The default transmitter power is 13dBm, using PA_BOOST.
+    rf95.setTxPower(20, false); // The default transmitter power is 13dBm, using PA_BOOST. - If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then you can set transmitter powers from 2 to 20 dBm:
 
-    // Stepper Motors
-    motor1.setMaxSpeed(4000);
-    motor1.setAcceleration(100);
-    motor2.setMaxSpeed(4000);
-    motor2.setAcceleration(100);
-
-    pinMode(6, OUTPUT); // M0 - Setting the step pin to high to initialize full step control
-    digitalWrite(6, LOW);
-    pinMode(7, OUTPUT); // M1 - Setting the step pin to high to initialize full step control
-    digitalWrite(7, LOW);
-
-    // Servo Stuff
-    servo1.attach(18); // attaches the servo on pin 9 to the servo object
-    servo2.attach(19); // attaches the servo on pin 9 to the servo object
-    servo1.write(180); // Set Servos - Values are from 0 to 180
-    servo2.write(0);   // Set Servos - Values are from 0 to 180
-
-    motor1.moveTo(-100000);
-    motor2.moveTo(-100000);
+    motor1.setMaxSpeed(500);    // Nosecone Max Speed
+    motor1.setAcceleration(100); // Nosecone Accel
+    motor2.setMaxSpeed(500);    // Coupler Max Speed
+    motor2.setAcceleration(100); // Nosecone Accel
+    pinMode(6, OUTPUT);          // M0 - defining M0 pin for stepper motors
+    digitalWrite(6, LOW);        // M0 - setting the step pin to low to initialize full step control
+    pinMode(7, OUTPUT);          // M1 - defining M1 pin for stepper motors
+    digitalWrite(7, LOW);        // M1 - setting the step pin to low to initialize full step control
+    pinMode(10, OUTPUT);         // GPS - defining GPS pin to enable / disable
+    digitalWrite(10, LOW);       // GPS - setting the GPS pin to low to disable it for the flight
+    servo2.attach(18);           // attaches the servo on pin 9 to the servo object
+    servo1.attach(19);           // attaches the servo on pin 9 to the servo object
+    servo1.write(00);           // Set Servos - Values are from 0 to 180
+    servo2.write(180);             // Set Servos - Values are from 0 to 180
 }
 
 void loop()
@@ -109,8 +75,6 @@ void loop()
         gps.encode(Serial1.read());
     }
 
-    bmp.performReading(); // Altimeter
-
     if (rf95.available())
     {
         uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
@@ -118,16 +82,24 @@ void loop()
 
         if (rf95.recv(buf, &len)) // if "separate" message detected
         {
-            if (strcmp((char *)buf, "separate") == 0)
+            if (strcmp((char *)buf, "key:s") == 0)
             {
-                motor1.moveTo(1000);
-                motor2.moveTo(2000);
-                separate = true;          // set separate to true
-                currentmillis = millis(); // set a counter to current time (0)
+                motor1.moveTo(28000);   // moves stepper motor 1 (Nosecone)
+                motor2.moveTo(64000);   // moves stepper motor 2 (Coupler)
+                digitalWrite(10, HIGH); // Turns GPS on
+                separate = true;        // set separate to true
             }
             if (strcmp((char *)buf, "rocketpos") == 0)
             {
-                sendposition();
+                if (gps.location.isValid() && (gps.location.age() < 10000))
+                {
+                    sendposition();
+                }
+            }
+            if (strcmp((char *)buf, " ") == 0)
+            {
+                motor1.stop();
+                motor2.stop();
             }
             Serial.println((char *)buf);
         }
@@ -135,11 +107,11 @@ void loop()
 
     if (separate == true) // if separating
     {
-        if ((motor1.distanceToGo() == 0 && motor2.distanceToGo() == 0) && ((millis() - currentmillis) >= 10000)) // Checks if Stepper Motors & Time are at their respective endpoints
+        if ((motor1.distanceToGo() == 0 && motor2.distanceToGo() == 0)) // Checks if Stepper Motors & Time are at their respective endpoints
         {
-            separate = false;  // Stops this function from running
-            servo1.write(0);   // Set Servos - Values are from 0 to 180
-            servo2.write(180); // Set Servos - Values are from 0 to 180
+            separate = false;  // Stops this function from running more than once
+            servo1.write(180);   // Set Servos - Values are from 0 to 180
+            servo2.write(0); // Set Servos - Values are from 0 to 180
         }
     }
 
--- HandHeld Controller/src/main.cpp
+++ HandHeld Controller/src/main.cpp
@@ -2,41 +2,12 @@
 #include <RH_RF95.h>
 #include <TinyGPSPlus.h>
 #include <Adafruit_BMP3XX.h>
-//#define BMP_CS 6
-//#define SEALEVELPRESSURE_HPA (1021.3)
-//Adafruit_BMP3XX bmp; // bmp390
+
 RH_RF95 rf95(10, 8); // Singleton instance of the radio driver
 TinyGPSPlus gps;
 bool position = false;
-
 uint32_t printTime = 0;
-
 bool initialized = false;
-
-void setup()
-{
-    Serial.begin(9600);
-    Serial1.begin(9600);
-    if (rf95.init())
-    {
-        initialized = true;
-    }                           // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-   // if (!bmp.begin_SPI(BMP_CS)) // hardware SPI mode
-   // {
-   //     Serial.println("Could not find a valid BMP3 sensor, check wiring!");
-   //     while (true)
-   //         ;
-   // }
-    // Set up oversampling and filter initialization
-   // bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
-   // bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-   // bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
-   // bmp.setOutputDataRate(BMP3_ODR_50_HZ);
-    // rf95.setModemConfig(RH_RF95::Bw500Cr45Sf128); // You can change the modulation parameters with eg
-    // rf95.setTxPower(20, false); //The default transmitter power is 13dBm, using PA_BOOST. - If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then you can set transmitter powers from 2 to 20 dBm:
-    printTime = micros();
-    
-}
 
 bool keyk = false;
 bool keyq = false;
@@ -49,175 +20,167 @@ bool keyr = false;
 bool keyf = false;
 bool keyspace = false;
 int incomingByte = 0;
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial1.begin(9600);
+    if (rf95.init())
+    {
+        initialized = true;
+    }                           // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+    rf95.setTxPower(20, false); // The default transmitter power is 13dBm, using PA_BOOST. - If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then you can set transmitter powers from 2 to 20 dBm:
+    printTime = micros();
+}
+
 void loop()
 {
     uint32_t time = micros();
- //   if (!bmp.performReading())
- //   {
-  //      Serial.println("Failed to perform altimeter reading");
- //   }
-    //while (Serial1.available() > 0)
-   // {
-   //     gps.encode(Serial1.read());
+    // while (Serial1.available() > 0)
+    // {
+    //     gps.encode(Serial1.read());
     //}
 
     while (Serial.available() > 0)
     {
         incomingByte = Serial.read();
-    {
-        Serial.println(char(incomingByte));
-        if (incomingByte == 'k') 
         {
-            Serial.println("key:k");
-            keyk = true;
+            Serial.println(char(incomingByte));
+            if (incomingByte == 'k')
+            {
+                Serial.println("key:k");
+                keyk = true;
+            }
+            if (incomingByte == ' ')
+            {
+                Serial.println("spacebar");
+                keyspace = true;
+            }
+            if (incomingByte == 'q')
+            {
+                Serial.println("key:q");
+                keyq = true;
+            }
+            if (incomingByte == 'a')
+            {
+                Serial.println("key:a");
+                keya = true;
+            }
+            if (incomingByte == 'w')
+            {
+                Serial.println("key:w");
+                keyw = true;
+            }
+            if (incomingByte == 's')
+            {
+                Serial.println("key:s");
+                keys = true;
+            }
+            if (incomingByte == 'e')
+            {
+                Serial.println("key:e");
+                keye = true;
+            }
+            if (incomingByte == 'd')
+            {
+                Serial.println("key:d");
+                keyd = true;
+            }
+            if (incomingByte == 'r')
+            {
+                Serial.println("key:r");
+                keyr = true;
+            }
+            if (incomingByte == 'f')
+            {
+                Serial.println("key:f");
+                keyf = true;
+            }
         }
-        if (incomingByte == ' ') 
-        {
-            Serial.println("spacebar");
-            keyspace = true;
-            }
-             if (incomingByte == 'q') 
-        {
-            Serial.println("key:q");
-            keyq = true;
-            }
-             if (incomingByte == 'a') 
-        {
-            Serial.println("key:a");
-            keya = true;
-            }
-             if (incomingByte == 'w') 
-        {
-            Serial.println("key:w");
-            keyw = true;
-            }
-             if (incomingByte == 's') 
-        {
-            Serial.println("key:s");
-            keys = true;
-            }
-             if (incomingByte == 'e') 
-        {
-            Serial.println("key:e");
-            keye = true;
-            }
-             if (incomingByte == 'd') 
-        {
-            Serial.println("key:d");
-            keyd = true;
-            }
-             if (incomingByte == 'r') 
-        {
-            Serial.println("key:r");
-            keyr = true;
-            }
-             if (incomingByte == 'f') 
-        {
-            Serial.println("key:f");
-            keyf = true;
-            }
-    }
     }
 
     if (keyk == true)
     {
         uint8_t data[] = "key:k";
-            rf95.send(data, sizeof(data));
-            rf95.waitPacketSent();
-            Serial.print("Sent key:k to rf95_server: ");
-            keyk = false;
+        rf95.send(data, sizeof(data));
+        rf95.waitPacketSent();
+        Serial.print("Sent key:k to rf95_server: ");
+        keyk = false;
     }
 
     if (keyspace == true)
     {
         uint8_t data[] = "key: ";
-            rf95.send(data, sizeof(data));
-            rf95.waitPacketSent();
-            Serial.print("Sent key:space to rf95_server: ");
-            keyspace = false;
+        rf95.send(data, sizeof(data));
+        rf95.waitPacketSent();
+        Serial.print("Sent key:space to rf95_server: ");
+        keyspace = false;
     }
 
-        if (keyq == true)
+    if (keyq == true)
     {
         uint8_t data[] = "key:q";
-            rf95.send(data, sizeof(data));
-            rf95.waitPacketSent();
-            Serial.print("Sent key:q to rf95_server: ");
-            keyq = false;
+        rf95.send(data, sizeof(data));
+        rf95.waitPacketSent();
+        Serial.print("Sent key:q to rf95_server: ");
+        keyq = false;
     }
-        if (keya == true)
+    if (keya == true)
     {
         uint8_t data[] = "key:a";
-            rf95.send(data, sizeof(data));
-            rf95.waitPacketSent();
-            Serial.print("Sent key:a to rf95_server: ");
-            keya = false;
+        rf95.send(data, sizeof(data));
+        rf95.waitPacketSent();
+        Serial.print("Sent key:a to rf95_server: ");
+        keya = false;
     }
-        if (keyw == true)
+    if (keyw == true)
     {
         uint8_t data[] = "key:w";
-            rf95.send(data, sizeof(data));
-            rf95.waitPacketSent();
-            Serial.print("Sent key:w to rf95_server: ");
-            keyw = false;
+        rf95.send(data, sizeof(data));
+        rf95.waitPacketSent();
+        Serial.print("Sent key:w to rf95_server: ");
+        keyw = false;
     }
-        if (keys == true)
+    if (keys == true)
     {
         uint8_t data[] = "key:s";
-            rf95.send(data, sizeof(data));
-            rf95.waitPacketSent();
-            Serial.print("Sent key:s to rf95_server: ");
-            keys = false;
+        rf95.send(data, sizeof(data));
+        rf95.waitPacketSent();
+        Serial.print("Sent key:s to rf95_server: ");
+        keys = false;
     }
-        if (keye == true)
+    if (keye == true)
     {
         uint8_t data[] = "key:e";
-            rf95.send(data, sizeof(data));
-            rf95.waitPacketSent();
-            Serial.print("Sent key:e to rf95_server: ");
-            keye = false;
+        rf95.send(data, sizeof(data));
+        rf95.waitPacketSent();
+        Serial.print("Sent key:e to rf95_server: ");
+        keye = false;
     }
-if (keyd == true)
+    if (keyd == true)
     {
         uint8_t data[] = "key:d";
-            rf95.send(data, sizeof(data));
-            rf95.waitPacketSent();
-            Serial.print("Sent key:d to rf95_server: ");
-            keyd = false;
+        rf95.send(data, sizeof(data));
+        rf95.waitPacketSent();
+        Serial.print("Sent key:d to rf95_server: ");
+        keyd = false;
     }
     if (keyr == true)
     {
         uint8_t data[] = "key:r";
-            rf95.send(data, sizeof(data));
-            rf95.waitPacketSent();
-            Serial.print("Sent key:r to rf95_server: ");
-            keyr = false;
+        rf95.send(data, sizeof(data));
+        rf95.waitPacketSent();
+        Serial.print("Sent key:r to rf95_server: ");
+        keyr = false;
     }
     if (keyf == true)
     {
         uint8_t data[] = "key:f";
-            rf95.send(data, sizeof(data));
-            rf95.waitPacketSent();
-            Serial.print("Sent key:f to rf95_server: ");
-            keyf = false;
+        rf95.send(data, sizeof(data));
+        rf95.waitPacketSent();
+        Serial.print("Sent key:f to rf95_server: ");
+        keyf = false;
     }
-        
-        /* OLD VERSION
-        char serialInput = Serial.read();
-
-        if (strcmp(&serialInput, "k") == 0)
-        {
-            
-        }
-
-        if (strcmp(&serialInput, " ") == 0)
-        {
-            uint8_t data[] = "key: ";
-            rf95.send(data, sizeof(data));
-            rf95.waitPacketSent();
-            Serial.print("Sending to rf95_server: ");
-        }
-*/
 
     if (rf95.available())
     {
@@ -229,42 +192,4 @@ if (keyd == true)
             Serial.println((char *)buf);
         }
     }
-
-
-    /*
-    if (position == true)
-    {
-        uint8_t data[24];
-
-        String lat = String("hlat") + String(gps.location.lat(), 8);
-
-        for (int i = 0; i < lat.length(); i++)
-        {
-            data[i] = lat.charAt(i);
-        }
-        rf95.send(data, lat.length());
-
-        delay(5);
-
-        String lng = String("hlng") + String(gps.location.lng(), 8);
-        for (int i = 0; i < lng.length(); i++)
-        {
-            data[i] = lng.charAt(i);
-        }
-        rf95.send(data, lng.length());
-
-        delay(5);
-
-        String alt = String("halt") + String(bmp.readAltitude(SEALEVELPRESSURE_HPA) * 3.28084, 8);
-        for (int i = 0; i < lng.length(); i++)
-        {
-            data[i] = alt.charAt(i);
-        }
-        rf95.send(data, alt.length());
-
-        delay(5);
-
-        position = false;
-    }
-*/
 }
